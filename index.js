b3a240cfebe7ba874e442f802f25d62d5e9d96d3
Federico <federico_mameli@yahoo.com>
2021-07-27 13:15:42 +0200

    feat: add service plugin file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+
 const ZipWebpackPlugin = require('zip-webpack-plugin')
 const LandingParamsPlugin = require('@smart-contact/landing-params-webpack-plugin')
 
@@ -6,10 +8,42 @@ module.exports = (api, options) => {
   const landingConfig = require(api.resolve('./landing.config.js'))
   const landingParams = JSON.stringify(fs.readFileSync(api.resolve('./landing-params.json')))
 
-  //add zip plugin
+  const buildFilenameTemplate = (ext) => `[name]-[hash:8].${ext}`
+
   api.chainWebpack(webpackConfig => {
-    //add zip plugin
-    if(process.env.NODE_ENV === 'production'){
+    webpackConfig
+      .output
+        .filename(buildFilenameTemplate('js'))
+        .chunkFilename(buildFilenameTemplate('js'))
+        .publicPath(IS_PRODUCTION ? `https://smart-contact-cdn.livelanding.it/${landingConfig.name}` : '/')
+
+    webpackConfig.devtool(IS_PRODUCTION ? false : 'source-map')
+    
+    webpackConfig
+      .plugin("landing-params")
+      .use(LandingParamsPlugin, [
+        {
+          params: landingParams
+        }
+      ])
+      .after("html");
+
+
+    //development only
+    if(!IS_PRODUCTION){
+
+    }
+
+    //production only
+    if(IS_PRODUCTION){
+      webpackConfig
+        .plugin('extract-css')
+        .tap(args => {
+          args[0].filename = buildFilenameTemplate('css')
+          args[0].chunkFilename = buildFilenameTemplate('css')
+          return args
+        })
+
       webpackConfig
         .plugin('zip-plugin')
         .use(ZipWebpackPlugin, [
@@ -19,16 +53,7 @@ module.exports = (api, options) => {
             pathPrefix: landingConfig.name
           }
         ])
+        
     }
-
-    //add landing-params plugin
-    config
-      .plugin("landing-params")
-      .use(LandingParamsPlugin, [
-        {
-          params: landingParams
-        }
-      ])
-      .after("html");
   })
 }
