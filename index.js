fd8667b88a0eb26ed6d61e74c33766314284b1a2
Federico <federico_mameli@yahoo.com>
2021-07-28 10:37:42 +0200

    fix(service-plugin):
    fix bug landing params & mod webpack production config

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const fs = require('fs')
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
-
 const ZipWebpackPlugin = require('zip-webpack-plugin')
 const LandingParamsPlugin = require('@smart-contact/landing-params-webpack-plugin')
 
 module.exports = (api, options) => {
   const landingConfig = require(api.resolve('./landing.config.js'))
-  const landingParams = JSON.stringify(fs.readFileSync(api.resolve('./landing-params.json')))
+  const landingParams = require(api.resolve('./landing-params.json'))
 
   const buildFilenameTemplate = (ext) => `[name]-[hash:8].${ext}`
 
@@ -15,9 +12,9 @@ module.exports = (api, options) => {
       .output
         .filename(buildFilenameTemplate('js'))
         .chunkFilename(buildFilenameTemplate('js'))
-        .publicPath(IS_PRODUCTION ? `https://smart-contact-cdn.livelanding.it/${landingConfig.name}` : '/')
+        .publicPath('/')
 
-    webpackConfig.devtool(IS_PRODUCTION ? false : 'source-map')
+    webpackConfig.devtool('source-map')
     
     webpackConfig
       .plugin("landing-params")
@@ -29,13 +26,11 @@ module.exports = (api, options) => {
       .after("html");
 
 
-    //development only
-    if(!IS_PRODUCTION){
-
-    }
-
     //production only
-    if(IS_PRODUCTION){
+    if(process.env.NODE_ENV === 'production'){
+      webpackConfig.output.publicPath(`https://smart-contact-cdn.livelanding.it/${landingConfig.name}`)
+      webpackConfig.devtool(false)
+
       webpackConfig
         .plugin('extract-css')
         .tap(args => {
@@ -56,4 +51,6 @@ module.exports = (api, options) => {
         
     }
   })
+
+
 }
