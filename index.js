397197c3a81ad0d74eb2a928c5550eef355e23df
Federico <federico_mameli@yahoo.com>
2021-07-29 14:31:35 +0200

    feat(service): change output for images/media/fonts and add imagemin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ZipWebpackPlugin = require("zip-webpack-plugin")
 const LandingParamsPlugin = require("@smart-contact/landing-params-webpack-plugin")
+const ImageminPlugin = require("imagemin-webpack-plugin").default
 
 module.exports = (api, options) => {
 	const landingConfig = require(api.resolve("./landing.config.js"))
@@ -13,10 +14,6 @@ module.exports = (api, options) => {
 			.filename(buildFilenameTemplate("js"))
 			.chunkFilename(buildFilenameTemplate("js"))
         
-		webpackConfig.output.publicPath = process.env.NODE_ENV === "production" 
-			? `${options.baseCdnUrl}/${landingConfig.name}` 
-			: "/"
-
 		webpackConfig.devtool("source-map")
     
 		webpackConfig
@@ -31,8 +28,47 @@ module.exports = (api, options) => {
 
 		//production only
 		if(process.env.NODE_ENV === "production"){
+			webpackConfig.output.publicPath = `${options.baseCdnUrl}/${landingConfig.name}` 
+			
 			webpackConfig.devtool(false)
 
+
+			//modify images
+			webpackConfig.module
+				.rule("images")
+				.use("url-loader")
+				.tap(args => {
+					args.fallback.options.name = "[name].[ext]"
+					return args
+				})
+        
+			webpackConfig.module
+				.rule("svg")
+				.use("file-loader")
+				.tap(args => {
+					args.name = "[name].[ext]"
+					return args
+				})
+
+			webpackConfig.module
+				.rule("media")
+				.use("url-loader")
+				.tap(args => {
+					args.fallback.options.name = "[name].[ext]"
+
+					return args
+				})
+
+			webpackConfig.module
+				.rule("fonts")
+				.use("url-loader")
+				.tap(args => {
+					args.fallback.options.name = "[name].[ext]"
+
+					return args
+				})
+
+			//modify css
 			webpackConfig
 				.plugin("extract-css")
 				.tap(args => {
@@ -40,6 +76,27 @@ module.exports = (api, options) => {
 					args[0].chunkFilename = buildFilenameTemplate("css")
 					return args
 				})
+			
+			//add images optimization
+			webpackConfig
+				.plugin("image-min")
+				.after("copy")
+				.use(ImageminPlugin, [
+					{
+						test: /\.(jpe?g|png|gif|svg)$/i,
+						jpegtran: {
+							progressive: true,
+							arithmetic: true
+						},
+						optipng: {
+							optimizationLevel: 4
+						},
+						gifsicle: {
+							optimizationLevel: 4
+						},
+						svgo: {}
+					}
+				])
 
 			webpackConfig
 				.plugin("zip-plugin")
