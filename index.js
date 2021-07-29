a76f7f069350450192c767fff0e04fbcc31e965a
Federico <federico_mameli@yahoo.com>
2021-07-29 11:16:56 +0200

    fix(service): fixes build publicPath error

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
-const ZipWebpackPlugin = require('zip-webpack-plugin')
-const LandingParamsPlugin = require('@smart-contact/landing-params-webpack-plugin')
+const ZipWebpackPlugin = require("zip-webpack-plugin")
+const LandingParamsPlugin = require("@smart-contact/landing-params-webpack-plugin")
 
 module.exports = (api, options) => {
-  const landingConfig = require(api.resolve('./landing.config.js'))
-  const landingParams = require(api.resolve('./landing-params.json'))
+	const landingConfig = require(api.resolve("./landing.config.js"))
+	const landingParams = require(api.resolve("./landing-params.json"))
 
-  const buildFilenameTemplate = (ext) => `[name]-[hash:8].${ext}`
+	const buildFilenameTemplate = (ext) => `[name]-[hash:8].${ext}`
 
-  api.chainWebpack(webpackConfig => {
-    webpackConfig
-      .output
-        .filename(buildFilenameTemplate('js'))
-        .chunkFilename(buildFilenameTemplate('js'))
-        .publicPath(process.env.NODE_ENV === 'production' ? `https://smart-contact-cdn.livelanding.it/${landingConfig.name}` : '/')
-
-    webpackConfig.devtool('source-map')
-    
-    webpackConfig
-      .plugin("landing-params")
-      .use(LandingParamsPlugin, [
-        {
-          params: landingParams
-        }
-      ])
-      .after("html");
-
-
-    //production only
-    if(process.env.NODE_ENV === 'production'){
-      webpackConfig.devtool(false)
-
-      webpackConfig
-        .plugin('extract-css')
-        .tap(args => {
-          args[0].filename = buildFilenameTemplate('css')
-          args[0].chunkFilename = buildFilenameTemplate('css')
-          return args
-        })
-
-      webpackConfig
-        .plugin('zip-plugin')
-        .use(ZipWebpackPlugin, [
-          {
-            filename: "dist.zip",
-            exclude: [/\.html$/],
-            pathPrefix: landingConfig.name
-          }
-        ])
+	api.chainWebpack(webpackConfig => {
+		webpackConfig
+			.output
+			.filename(buildFilenameTemplate("js"))
+			.chunkFilename(buildFilenameTemplate("js"))
         
-    }
-  })
+		webpackConfig.output.publicPath = process.env.NODE_ENV === "production" 
+			? `${options.baseCdnUrl}/${landingConfig.name}` 
+			: "/"
+
+		webpackConfig.devtool("source-map")
+    
+		webpackConfig
+			.plugin("landing-params")
+			.use(LandingParamsPlugin, [
+				{
+					params: landingParams
+				}
+			])
+			.after("html")
+
+
+		//production only
+		if(process.env.NODE_ENV === "production"){
+			webpackConfig.devtool(false)
+
+			webpackConfig
+				.plugin("extract-css")
+				.tap(args => {
+					args[0].filename = buildFilenameTemplate("css")
+					args[0].chunkFilename = buildFilenameTemplate("css")
+					return args
+				})
+
+			webpackConfig
+				.plugin("zip-plugin")
+				.use(ZipWebpackPlugin, [
+					{
+						filename: "dist.zip",
+						exclude: [/\.html$/],
+						pathPrefix: landingConfig.name
+					}
+				])
+        
+		}
+	})
 
 
 }
