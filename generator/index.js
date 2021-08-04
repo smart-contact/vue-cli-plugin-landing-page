7d3725fd57a2428aa75cb295d0a165cca98ccd3d
Federico <federico_mameli@yahoo.com>
2021-08-04 15:57:38 +0200

    refact: add style lint deps & force deps versions

--- generator/index.js
+++ generator/index.js
@@ -7,16 +7,20 @@ module.exports = function(api, options){
 	//adding smart-contact packages + extra deps.
 	api.extendPackage({
 		dependencies: {
-			"@smart-contact/comparatore-api-service": "^2.x",
+			"@smart-contact/comparatore-api-service": "^2.4.1",
 			"@smart-contact/smartify": "^0.x",
 			// '@smart-contact/smartland': '^3.x',
-			"@smart-contact/landing-js": "^2.7.x",
-			"@smart-contact/validators": "^1.x",
+			"@smart-contact/landing-js": "^2.7.3",
+			"@smart-contact/validators": "^1.1.0",
 			"vuelidate": "^0.7.x"
 		},
 		devDependencies: {
-			"@smart-contact/landing-params-webpack-plugin": "^1.x",
-			"zip-webpack-plugin": "^4.x"
+			"@smart-contact/landing-params-webpack-plugin": "^1.2.1",
+			"stylelint": "^13.13.1",
+			"stylelint-config-recommended-scss": "^4.3.0",
+			"stylelint-scss": "^3.20.1",
+			"stylelint-webpack-plugin": "^3.0.1",
+			"zip-webpack-plugin": "^4.0.1"
 		}
 	})
 
