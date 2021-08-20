02136a14ffd5c0d5c8ba24451c153364ba621396
Federico <federico_mameli@yahoo.com>
2021-08-20 14:56:14 +0200

    fix(service): filename bundle

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = (api, options) => {
 	const landingConfig = require(api.resolve("./landing.config.js"))
 	const landingParams = require(api.resolve("./landing-params.json"))
 
-	const buildFilenameTemplate = (ext) => `[name]-[hash:8].${ext}`
+	const buildFilenameTemplate = (ext) => `[name]-[contenthash].${ext}`
 
 	//inject all variables/functions/mixins to all vue sfc components
 	options.css = merge(options.css, {
