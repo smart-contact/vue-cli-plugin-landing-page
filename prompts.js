fab0068b01c2495bb1460d126a2c05e63ee48afb
Federico <federico_mameli@yahoo.com>
2021-08-20 10:45:42 +0200

    feat(prompts): add questions for vuex & router

--- prompts.js
+++ prompts.js
@@ -3,5 +3,21 @@ module.exports = [
 		type: "text",
 		name: "cdnBaseURL",
 		default: "https://smart-contact-cdn.livelanding.it"
+	},
+	{
+		type: "confirm",
+		name: "useRouter",
+		default: false
+	},
+	{
+		type: "confirm",
+		name: "useVuex",
+		default: false	
+	},
+	{
+		type: "confirm",
+		name: "useProductsVuexModule",
+		default: false,
+		message: "Do you want to use products vuex module instead of compositionApi?"
 	}
 ]
